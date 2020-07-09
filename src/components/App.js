833323b5b9e503929337fca17b9eda15200f877b
Rishabh Das <rishabhdasufl@gmail.com>
2020-07-09 07:22:00 +0530

    Add stream component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const PageOne = () => {
+  return <div>1</div>;
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      2<button>Click me</button>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div> App </div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
