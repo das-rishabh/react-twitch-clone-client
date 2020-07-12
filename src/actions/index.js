d42ebf5927fd2905a2045d088658fdf8be5cf10f
Rishabh Das <rishabhdasufl@gmail.com>
2020-07-12 11:32:24 +0530

    Bulk action creation

--- src/actions/index.js
+++ src/actions/index.js
@@ -17,3 +17,23 @@ export const createStream = (formValues) => async (dispatch) => {
   const response = await streams.post("/streams", formValues);
   dispatch({ type: "CREATE_STREAM", payload: response.data });
 };
+
+export const fetchStreams = () => async (dispatch) => {
+  const response = await streams.post(`/streams/${id}`);
+  dispatch({ type: "FETCH_STREAMS", payload: response.data });
+};
+
+export const fetchStream = (id) => async (dispatch) => {
+  const response = await streams.post(`/streams/${id}`);
+  dispatch({ type: "FETCH_STREAM", payload: response.data });
+};
+
+export const editStream = (id, formValues) => async (dispatch) => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+  dispatch({ type: "EDIT_STREAM", payload: response.data });
+};
+
+export const deleteStreams = (id) => async (dispatch) => {
+  await streams.delete(`/streams/${id}`);
+  dispatch({ type: "DELETE_STREAM", payload: id });
+};
